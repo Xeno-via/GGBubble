629b8c1dc9f5a3884e78dd636a06d935d725eb2f
GeorgeG94 <george_gilliver@hotmail.co.uk>
2023-12-04 17:30:05 +0000

    stupid system of making settings without touching code :3

---
+++ ReadSettings.py
@@ -0,0 +1,11 @@
+def read_line(file):
+    return file.readline().strip().split('=')[1]
+
+def read_settings():
+    Settings = {}
+    with open("settings.txt", 'r') as file:
+        Settings["seed"] = int(read_line(file))
+        Settings["font_size"] = int(read_line(file))
+    return Settings
+
+
